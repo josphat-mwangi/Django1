fe132c6975e8d7af4197fe6baebc21bf2cad5cfb
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-03 09:52:19 +0300

    add image models and do migration

--- photos/models.py
+++ photos/models.py
@@ -15,11 +15,11 @@ class Category(models.Model):
         return self.name
 
 class Image(models.Model):
-    # image=models.ImageField(upload_to='images/')
     image_name=models.CharField(max_length=100)
     image_description=models.TextField()
     image_location=models.ForeignKey(Location,on_delete=models.CASCADE)
     image_category=models.ManyToManyField(Category)
+    image = models.ImageField(upload_to = 'images/')
 
     def __str__(self):
         return self.image_name
