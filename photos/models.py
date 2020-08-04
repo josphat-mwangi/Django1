5020f4db65808913845e620cdb684dc240bf7d44
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-05 01:19:27 +0300

    add test

--- photos/models.py
+++ photos/models.py
@@ -4,11 +4,30 @@ from django.db import models
 class Location(models.Model):
     name=models.CharField(max_length=200)
 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()  
+
     def __str__(self):
         return self.name
 
 class Category(models.Model):
     name=models.CharField(max_length=200)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
 
     def __str__(self):
         return self.name
@@ -21,6 +40,12 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images/', blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
     @classmethod
     def photo_display(cls):
        photos = cls.objects.filter()
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
-
+from .models import Image,Category,Location
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.nairobi = Location(name='Kenya')
+        self.nairobi.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+    
+    
+    
+    def tearDown(self):
+        self.nairobi.delete_location() 
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.cupsi = Category(name='Cars')
+        self.cupsi.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cupsi,Category))
+    
+    def tearDown(self):
+        self.cupsi.delete_category()
+    
+
+class ImageTestClass(TestCase):
+  
+   def setUp(self):
+        self.jump = Category(name='cars')
+        self.jump.save_category()
+
+        self.nairobi = Location(name='Nairobi')
+        self.nairobi.save_location()
+
+        self.image = Image(image_name='nature', image_description='have a good one', location=self.nairobi, category=self.jump)
+        self.image.save_image()
+   def test_instance(self):
+       self.assertTrue(isinstance(self.image, Image))
+
+   def test_save_method(self):
+       self.image.save_image()
+       images = Image.objects.all()
+       self.assertTrue(len(images) > 0)
+
+   def test_delete_method(self):
+    
+       self.image.save_image()
+       self.image.delete_image()
+
+   def test_update_method(self):
+    
+       self.image.save_image()
+       new_image = Image.objects.filter(image='image1.jpg').update(photo='download.jpg')
+       images = Image.objects.get(image='download.jpg')
+       self.assertTrue(images.image, 'download.jpg')
+
+   def test_get_image_by_id(self):
+    
+       self.image.save_image()
+       this_img= self.image.get_image_by_id(self.image.id)
+       image = Image.objects.get(id=self.image.id)
+       self.assertTrue(this_img, image)
+
+   def test_filter_by_location(self):
+      
+       self.image.save_image()
+       this_img = self.image.filter_by_location(self.image.location_id)
+       image = Image.objects.filter(location=self.image.location_id)
+       self.assertTrue(this_img, image)
+
+   def test_filter_by_category_name(self):
+      
+       self.image.save_image()
+       images = Image.search_image('this')
+       self.assertTrue(len(images)>0)
+          
+                     
